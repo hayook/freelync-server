59216255c07336b7629f63f15dc7169c67d7afd3
hayook <hay.eddine8@gmail.com>
2025-01-30 18:29:19 +0100

    removed unused variables

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cors = require('cors');
 const sequelize = require('./config/database');
 const User = require('./models/User');
 const Gig = require('./models/Gig');
-const { build } = require('./models/User');
 // Relationships
 User.hasMany(Gig);
 Gig.belongsTo(User);
@@ -22,8 +21,8 @@ app.use(express.json());
 app.use('/api/gigs', require('./routes/api-router'));
 app.use('/users', require('./routes/users-router'));
 app.use(require('./routes/auth-router'));
-app.get('/', (req, res) => res.status(200).end('Home'));
-app.get('*', (req, res) => res.sendStatus(404));
+app.get('/', (_, res) => res.status(200).end('Home'));
+app.get('*', (_, res) => res.sendStatus(404));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started On http://localhost:${PORT}`));   
